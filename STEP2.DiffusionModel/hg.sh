499d940c3352dd0df90f2ed23a02acf7d02f7e5b
Qi Chen <105594482+qic999@users.noreply.github.com>
2025-06-14 16:02:56 +0800

    Update hg.sh

--- STEP2.DiffusionModel/hg.sh
+++ STEP2.DiffusionModel/hg.sh
@@ -27,6 +27,6 @@ fold=0
 datapath=/scratch/zzhou82/data/Task03_Liver/
 tumorlabel=/scratch/zzhou82/data/preprocessed_labels/
 
-python train.py dataset.name=liver_tumor_train dataset.fold=$fold dataset.data_root_path=$datapath dataset.label_root_path=$tumorlabel dataset.dataset_list=['liver_tumor_data_early_fold'] dataset.uniform_sample=False model.results_folder_postfix="liver_early_tumor_fold'$fold'"  model.vqgan_ckpt=$vqgan_ckpt
+python train.py dataset.name=liver_tumor_train dataset.fold=$fold dataset.data_root_path=$datapath dataset.label_root_path=$tumorlabel dataset.dataset_list=['liver_tumor_data_early_fold'] dataset.uniform_sample=False model.results_folder_postfix="liver_early_tumor_fold"$fold""  model.vqgan_ckpt=$vqgan_ckpt
 
 # sbatch --error=logs/diffusion_model.out --output=logs/diffusion_model.out hg.sh
